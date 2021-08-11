c33f67da41615b9fd2ec9ac1778f04e498d50c01
Machina Speculatrix <47831519+mspeculatrix@users.noreply.github.com>
2021-08-11 22:12:09 +0200

    added samples
    Added sample config files

---
+++ sample_InfluxDbConfig.h
@@ -0,0 +1,4 @@
+#define INFLUXDB_URL "http://10.0.30.50:8086"
+#define INFLUXDB_DB_NAME "db_name"
+#define INFLUXDB_USER "db_user"
+#define INFLUXDB_PASSWORD "password"
---
+++ sample_WifiCfgHome.h
@@ -0,0 +1,3 @@
+#define WIFI_SSID_MAIN "ssid1"
+#define WIFI_SSID_ALT "ssid2"
+#define WIFI_PASSWORD "password"
